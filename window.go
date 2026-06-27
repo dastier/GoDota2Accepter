11e0650185f73a31cb0bf7e56a4489f4f29905a7
dastier <petrovsky.lexey@gmail.com>
2026-06-27 13:56:41 +0200

    refactor: manage DBus listener with context and graceful shutdown (#7)
    * refactor: manage DBus listener with context and graceful shutdown
    
    * add go mod tidy step
    
    * fix: resolve listener build failures
    
    ---------
    
    Co-authored-by: Aliaksei Piatrouski <aliaksei.piatrouski@splitmetrics.com>

--- window.go
+++ window.go
@@ -6,26 +6,22 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
-
-
-func findIds(id string) {
+func findIds(id string) error {
 	// find the process id by the process name
 	fmt.Println("we are in findIds")
 	sum := 1
 	for sum < 10 {
 		sum += sum
 
-	
 		fpid, err := robotgo.FindIds(id)
 
 		fmt.Println("fpid: ", fpid)
 		if err == nil {
 			fmt.Println("pids...", fpid)
 			if len(fpid) > 0 {
-				if robotgo.ActivePID(fpid[0]) != nil {
-					fmt.Println("Could not find dota window", err)
+				if err := robotgo.ActivePID(fpid[0]); err != nil {
+					return fmt.Errorf("could not find dota window: %w", err)
 				}
-
 
 				tl := robotgo.GetTitle(fpid[0])
 				fmt.Println("pid[0] title is: ", tl)
@@ -42,7 +38,9 @@ func findIds(id string) {
 			}
 		} else {
 			fmt.Println("Found errors while robotgo.FindIds:", err)
+			return fmt.Errorf("found errors while robotgo.FindIds: %w", err)
 		}
 
-}
+	}
+	return nil
 }
